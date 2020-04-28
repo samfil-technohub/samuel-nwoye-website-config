2add96597d3561c6fa80ded5c22adfeb59e59698
knoxknot <samuel.nwoye@yahoo.com>
2020-04-28 14:58:39 +0100

    chore: added server testing files

---
+++ serverspec/spec/localhost/server_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe package('git') do
+  it { is_expected.to be_installed }
+end
+
+describe package('serverspec') do
+  it { is_expected.to be_installed.by('gem') }
+end
+
+describe 'Go' do
+  it { expect(file '/usr/local/go/bin/go').to exist & be_executable }
+end
+
+describe 'Nginx' do
+  it 'is expected to be installed, running and enabled' do
+    expect(package 'nginx').to be_installed
+    expect(service 'nginx').to be_running & be_enabled
+  end
+end
+
+describe 'GoApp' do
+  it 'is expected to be running, enabled and listening on 80' do
+    expect(service 'goapp').to be_running & be_enabled
+    expect(port 80).to be_listening 'tcp'
+  end
+end
